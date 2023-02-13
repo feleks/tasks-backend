34b5cd1d1d529c3987db3b9d92044195a4fe55d2
feleks <nashvel@gmail.com>
2023-02-13 20:05:04 +0300

    Добавлены run файлы.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import os
+
 if __name__ == '__main__':
-    print('Hello world')
+    host = os.getenv('HTTP_SERVER_HOST')
+    port = os.environ.get('HTTP_SERVER_PORT')
+
+    print('Should start server on {}:{}'.format(host, port))
